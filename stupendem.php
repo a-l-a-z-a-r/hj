770258f4dd77a66ea1cacd57a86534b88d5cbd64
a-l-a-z-a-r <33756081+a-l-a-z-a-r@users.noreply.github.com>
2021-04-22 15:08:07 +0200

    Initial commit

---
+++ stupendem.php
@@ -0,0 +1,217 @@
+<html> 
+    <head> 
+        <title> Shoping site  </title>
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/glide.core.min.css">
+        <link rel="stylesheet" href="css/glide.theme.css">
+
+
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="css/shopping.css?<?php echo time(); ?>">
+    </head>
+    <body> 
+   
+        <nav class="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
+            <a class="navbar-brand" href="#">ClothSite</a>
+             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                     <ul class="navbar-nav">
+                     <li class="nav-item active">
+                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                     </li>
+                     <li class="nav-item">
+                     <a class="nav-link" href="#">Features</a>
+                     </li>
+                     <li class="nav-item">
+                     <a class="nav-link" href="#">Pricing</a>
+                     </li>
+                     <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Menu</a>
+                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                     <a class="dropdown-item" href="#">Shoes</a>
+                     <a class="dropdown-item" href="#">T-Shirts</a>
+                     <a class="dropdown-item" href="#">Something else here</a>
+                  </div>
+               </li>
+             </ul>
+         </div>
+        </nav>
+  
+
+
+            
+        <div class="container">
+        <div class="glide">
+            <div class="glide__track" data-glide-el="track">
+                <ul class="glide__slides">
+                <li class="glide__slide"><img src="img/emre.jpg" class="sd"></li>
+                <li class="glide__slide"><img src="img/carou7.jpg "  class="sd"></li>
+                <li class="glide__slide"><img src="img/carou8.jpg"  class="sd"></li>
+                <li class="glide__slide"><img src="img/carou9.jpg"  class="sd"></li>
+                <li class="glide__slide"><img src="img/carou15.jpg"  class="sd"></li>
+                <li class="glide__slide"><img src="img/carou16.jpg"  class="sd"></li>
+                <li class="glide__slide"><img src="img/carou12.jpg"  class="sd"></li>
+                </ul>
+                     <div class="glide">
+                    <div class="glide__track" data-glide-el="track"></div>
+
+                    <div class="glide__arrows" data-glide-el="controls">
+                        <button class="glide__arrow glide__arrow--left" data-glide-dir="<">prev</button>
+                        <button class="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
+                    </div>
+                    </div>
+            </div>
+            </div>
+            <div class="all ">
+>            <section class="one">
+            <div class="row">
+    <div class="col-md-4">
+      <div class="thumbnail">
+          <h1>Jackets</h1>
+          <button><a href="#" class="coolBeans"> </a> </button> 
+          <img src="img/Jackets.jpg">
+          <div class="caption">
+            <p>This Dream Jacket you have always been looking for.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+          <img src="img/Jackets.jpg" alt="Lights" style="width:100%">
+          <div class="caption">
+            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+          <img src="img/Jackets.jpg" alt="Lights" style="width:100%">
+          <div class="caption">
+            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+          </div>
+        </a>
+      </div>
+</div>
+</div >
+       
+</section>
+<section class="one">
+<div class="row">
+<div class="col-md-4">
+      <div class="thumbnail">
+          <h1>Jackets</h1>
+          <img src="img/Jackets.jpg">
+          <div class="caption">
+            <p>This Dream Jacket you have always been looking for.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+          <h1>Jackets</h1>
+          <img src="img/Jackets.jpg">
+          <div class="caption">
+            <p>This Dream Jacket you have always been looking for.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+          <h1>Jackets</h1>
+          <img src="img/Jackets.jpg">
+          <div class="caption">
+            <p>This Dream Jacket you have always been looking for.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+</section> 
+
+<section class="one">
+<div class="row">
+<div class="col-md-4">
+      <div class="thumbnail">
+          <h1>Jackets</h1>
+          <img src="img/Jackets.jpg">
+          <div class="caption">
+            <p>This Dream Jacket you have always been looking for.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+          <h1>Jackets</h1>
+          <img src="img/Jackets.jpg">
+          <div class="caption">
+            <p>This Dream Jacket you have always been looking for.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+          <h1>Jackets</h1>
+          <img src="img/Jackets.jpg">
+          <div class="caption">
+            <p>This Dream Jacket you have always been looking for.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+</section> 
+</div>
+
+<div class="hero-wrapper">
+      <div class="hero">
+        <figure>
+          <video loop="loop" muted="muted" autoplay="autoplay">
+            <source src="img/model.mp4"/>
+          </video> 
+        </figure>
+      </div>
+      
+    </div>
+
+  
+      
+
+        <script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
+       
+       
+       <script> 
+        var input = document.querySelector('#options-autoplay-input')
+
+           const change = {
+               type: 'carousel',
+               perView: 2,
+               breakpoints: {
+                   800:{
+                     perView: 2
+                   },
+                   500:{
+                     perView: 1
+                   }
+                  }
+           };
+          new Glide('.glide', change).mount()
+        </script>
+    
+           
+
+    </body> 
+</html> 
